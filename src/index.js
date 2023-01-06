873fd05dd59d17e97a99eefb18b1882886e0d090
tc1421 <tc1421@ic.ac.uk>
2023-01-06 12:00:58 +0100

    added image and font bundler

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import _ from 'lodash';
 import myName from './myName';
 import './style.css';
+import Icon from './testimg.jpg';
 
 function component() {
     const element = document.createElement('div');
     element.textContent = myName('Cody') + _.join(['hello', 'webpack'], ' ');
     element.classList.add('hello');
     
+
+    //add image
+    const myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
 
     return element;
 }
