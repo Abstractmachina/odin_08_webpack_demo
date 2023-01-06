7c05fe629b7e193806aa1c042e6c8393da47378a
tc1421 <tc1421@ic.ac.uk>
2023-01-06 12:25:18 +0100

    restore. Please enter the commit message for your changes. Lines starting

--- src/index.js
+++ src/index.js
@@ -1,25 +1,9 @@
 import _ from 'lodash';
 import myName from './myName';
-import './style.css';
-import Icon from './testimg.jpg';
-import Books from './dummy.xml';
-import Data from './dummy.csv';
 
 function component() {
     const element = document.createElement('div');
     element.textContent = myName('Cody') + _.join(['hello', 'webpack'], ' ');
-    element.classList.add('hello');
-    
-
-    //add image
-    const myIcon = new Image();
-    myIcon.src = Icon;
-
-    element.appendChild(myIcon);
-
-    console.log(Books);
-    console.log(Data);
-
     return element;
 }
 
