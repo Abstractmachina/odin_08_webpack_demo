ecc665c27a7fcbf3ce2c1722c99a8d75e1e78715
tc1421 <tc1421@ic.ac.uk>
2023-01-06 11:28:12 +0100

    init commit. adding from previous tutorial

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import _ from 'lodash';
+import myName from './myName';
+
+function component() {
+    const element = document.createElement('div');
+    element.textContent = myName('Cody');
+
+    return element;
+}
+
+document.body.appendChild(component());
