3e48166f487e5ea92ad67c05d53c38d1da41fd2d
tc1421 <tc1421@ic.ac.uk>
2023-01-06 11:43:46 +0100

    css bundler added.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import _ from 'lodash';
 import myName from './myName';
+import './style.css';
 
 function component() {
     const element = document.createElement('div');
-    element.textContent = myName('Cody');
+    element.textContent = myName('Cody') + _.join(['hello', 'webpack'], ' ');
+    element.classList.add('hello');
+    
 
     return element;
 }
